1c13d412462269712b894b1298dc2fd3e9c64f2c
Daniel Kol Adam <venomdrophearthstone@gmail.com>
2020-02-14 16:32:04 +0200

    routing

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,20 +2,21 @@ import React from "react";
 import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
   // constructor() {
   //   super();
   //   // bind goToStore so that it refers to the StorePicker instance
   //   this.goToStore = this.goToStore.bind(this);
   // }
   // creating a ref
-  myInput = React.createRef();
   // make go to store as a property of the class syntax
   goToStore = event => {
     // 1) stop the form from auto submit
     event.preventDefault();
     // 2) get the text from that input
-    console.log(this.myInput.value.value); // .value twice because one is react syntx the the other is js syntx
+    const storeName = this.myInput.current.value; // .current is react reference, .value is js method
     // 3) change the url to the input
+    this.props.history.push(`/store/${storeName}`);
   };
   render() {
     return (
